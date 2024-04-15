33eb167925e97dd5f21fe332749e58692ba8b826
Utsav <utsav2005maji@gmail.com>
2024-04-15 12:12:59 +0530

    Done, now deploying

---
+++ scraper2.py
@@ -0,0 +1,39 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+def linkedin_scraper(webpage, page_number):
+    file = open('data/job_data.csv', 'w')
+    writer = csv.writer(file)
+    writer.writerow(['title', 'company', 'location', 'link'])
+
+    next_page = webpage + str(page_number)
+    print(str(next_page))
+    response = requests.get(str(next_page))
+    soup = BeautifulSoup(response.content,'html.parser')
+
+    jobs = soup.find_all('div', class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
+    for job in jobs:
+        job_title = job.find('h3', class_='base-search-card__title').text.strip()
+        job_company = job.find('h4', class_='base-search-card__subtitle').text.strip()
+        job_location = job.find('span', class_='job-search-card__location').text.strip()
+        job_link = job.find('a', class_='base-card__full-link')['href']
+ 
+        writer.writerow([
+        job_title.encode('utf-8'),
+        job_company.encode('utf-8'),
+        job_location.encode('utf-8'),
+        job_link.encode('utf-8')
+        ])
+ 
+    print('Data updated')
+
+    if page_number < 50:
+        page_number = page_number + 1
+        linkedin_scraper(webpage, page_number)
+    else:
+        file.close()
+        print('File closed')
+# page_number=0
+# webpage='https://www.linkedin.com/jobs/search/?currentJobId=3876184250&geoId=102713980&keywords=data%20analyst&location=India&origin=JOB_SEARCH_PAGE_LOCATION_AUTOCOMPLETE&refresh=true'
+# linkedin_scraper(webpage, page_number)
---
+++ server.py
@@ -0,0 +1,31 @@
+from scraper2 import linkedin_scraper
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
+import uvicorn
+import pandas as pd
+
+app = FastAPI()
+
+@app.get("/run")
+async def run_scrapy():
+    page_number=0
+    webpage='https://www.linkedin.com/jobs/search/?currentJobId=3876184250&geoId=102713980&keywords=data%20analyst&location=India&origin=JOB_SEARCH_PAGE_LOCATION_AUTOCOMPLETE&refresh=true'
+    linkedin_scraper(webpage, page_number)
+    return {"message": "Process completed successfully"}
+
+@app.get("/get_csv")
+async def get_csv():
+    csv_file_path = "data/job_data.csv" 
+    df = pd.read_csv(csv_file_path)
+    csv_string = df.to_csv(index=False)
+    headers = {
+        "Content-Disposition": "attachment; filename=job_data.csv",
+        "Content-Type": "text/csv",
+    }
+    return Response(content=csv_string, status_code=200, headers=headers)
+
+def run_app():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    run_app()
